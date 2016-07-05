7e207d0f5ff041c28b536895f26a1bec7db73f68
Edav13 <Edav13@hotmail.com>
2016-07-04 22:20:05 -0400

    003 manejo de scope

--- js/Controller.js
+++ js/Controller.js
@@ -1,7 +1,9 @@
-angular.module("Primero", [])
+angular.module("Tercero", [])
 	.controller("AlumnoController", ["$scope", "$http", function($scope, $http) {
-		$scope.alumno = {
-				nombre: "Juan Blanco",
-				curso: "3° ESO"
-			}
+		$scope.alumnos = [
+			{nombre: "Juan Blanco", telefono: "1423543", curso:"Segundo ESO"},
+			{nombre: "Rosa Luxemburgo", telefono: "535443342", curso:"Primero ESO"},
+			{nombre: "Alberto Herrera", telefono: "23465656", curso:"Segundo ESO"},
+			{nombre: "Ana Mariño", telefono: "98655342", curso:"Tercero ESO"}
+			];
 	}]);
